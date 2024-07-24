14c18cc734a5f6c8b0383ccb453f698c2780a182
Reina2024 <simmsreina@gmail.com>
2024-07-24 19:38:48 -0400

    Added packages needed for app

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const colors = require("colors");
+const validator = require("validator");
+const boxen = require('boxen');
+const readmeGen = require("./utils/generateMarkdown.js");
+
+// Your code using inquirer, colors, validator, and readmeGen
 
 // TODO: Create an array of questions for user input
 const questions = [];
