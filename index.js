a4647dfb29851ea1c0e1363cb66800c0176026ce
Reina2024 <simmsreina@gmail.com>
2024-07-24 19:53:30 -0400

    added questions and licenses to js

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const readmeGen = require("./utils/generateMarkdown.js");
 // Your code using inquirer, colors, validator, and readmeGen
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+"Enter your username",
+"Enter your email",
+"Title of the Project?",
+"Descrpition:",
+"Installation Guidance?",
+"Licences?",
+"Contributions",
+"Tests and Testing",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -18,3 +27,13 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+// Licenses selections
+const licenses = [
+    "WTFPL",
+    "MIT",
+    "BSD 3-Clause",
+    "CC0",
+    "GNU GPL v3",
+    "No license"
+  ]
