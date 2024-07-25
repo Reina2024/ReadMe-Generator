db44d72f8b064642680732fc22f9ada89e7a21c2
Reina2024 <simmsreina@gmail.com>
2024-07-24 21:10:03 -0400

    add function to generate markdown for readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -37,16 +37,31 @@ function renderLicenseLink(license) {
       case "No license":
         return "";
 }
-
-// TODO: Create a function that returns the license section of README
+  }
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  fullLicense = `[${renderLicenseBadge(license)}](${renderLicenseLink(license)})`;
+  return fullLicense;
+  }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
 }
 
-module.exports = generateMarkdown;
+//function to generate markdown for README
+function generateMarkdown(data) {
+  const {username, email, title, description, installation, usage, license, contributing, tests} = data;
+
+  return renderTitleSection(title, license) + 
+  insertTableOfContent(title) +
+  renderCommonSection("Description" , description) + 
+  renderCommonSection("Installation" , installation) + 
+  renderCommonSection("Usage" , usage) +
+  renderCommonSection("Contributing" , contributing) + 
+  renderCommonSection("Tests" , tests) +
+  renderLicenseSection(license) + 
+  renderQuestionsSection(username, email);
+}
+
+
+
+module.exports = {generateMarkdown};
